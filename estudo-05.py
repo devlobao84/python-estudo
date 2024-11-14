1f73625bd4c30fd3597cf506ab92dd6c581170df
Lobão <web@emd.art.br>
2024-11-14 18:09:48 -0300

    estudo de funções nativas em python 0.2

---
+++ estudo-05.py
@@ -0,0 +1,30 @@
+import math 
+import os
+
+x = 16
+
+raiz_quad = math.sqrt(x)
+
+print("Raiz quadrada de", x, "é igual a", raiz_quad)
+
+
+angulo = 45
+seno = math.sin(angulo)
+
+print("O seno de", angulo, "é igual a", seno)
+
+diretorio = os.getcwd()
+print("O diretório corrente é", diretorio)
+
+
+lista = [11, 22, 33, 44]
+tam = len(lista)
+print("O tamanho da", lista, "é igual a", tam)
+
+lista = [11, 22, 33, 44]
+soma = sum(lista)
+print("A soma dessa lista", lista, "resultará em", soma)
+
+
+#os.system("cls")
+
